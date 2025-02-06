f99521cf58953004a1e64eac289bb9ed11435f71
aem2231 <mariojetullaji@gmail.com>
2025-02-06 14:32:30 +0000

    added contact button

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.getElementById("smallfetch-button").onclick = function () {
   window.location.href = "https://github.com/aem2231/smallfetch"
 };
+
+window.onload = function () {
+  document.getElementById("contact-popup").style.display = "none";
+};
+
+document.getElementById("contact-button").addEventListener("click", function () {
+  document.getElementById("contact-popup").style.display = "flex";
+});
+
+document.getElementById("close-contact-popup").addEventListener("click", function () {
+  document.getElementById("contact-popup").style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+  let popup = document.getElementById("contact-popup");
+  if (event.target === popup) {
+      popup.style.display = "none";
+  }
+});
