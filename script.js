cc301ac05e7312944199d2ca5c8bf8f106986012
aem2231 <mariojetullaji@gmail.com>
2026-01-20 13:18:17 +0000

    im losing it

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 window.onload = function () {
   document.getElementById("contact-popup").style.display = "none";
   document.getElementById("smallfetch-popup").style.display = "none";
-
+  document.getElementById("p2p-chat-popup").style.display = "none";
 };
 
-document.getElementById("contact-button").addEventListener("click", function () {
-  document.getElementById("contact-popup").style.display = "flex";
-});
+document
+  .getElementById("contact-button")
+  .addEventListener("click", function () {
+    document.getElementById("contact-popup").style.display = "flex";
+  });
 
-document.getElementById("close-contact-popup").addEventListener("click", function () {
-  document.getElementById("contact-popup").style.display = "none";
-});
+document
+  .getElementById("close-contact-popup")
+  .addEventListener("click", function () {
+    document.getElementById("contact-popup").style.display = "none";
+  });
 
 window.addEventListener("click", function (event) {
   let popup = document.getElementById("contact-popup");
   if (event.target === popup) {
-      popup.style.display = "none";
+    popup.style.display = "none";
   }
 });
 
-document.getElementById("smallfetch-button").addEventListener("click", function () {
-  document.getElementById("smallfetch-popup").style.display = "flex";
-});
+document
+  .getElementById("smallfetch-button")
+  .addEventListener("click", function () {
+    document.getElementById("smallfetch-popup").style.display = "flex";
+  });
 
-document.getElementById("close-project-popup").addEventListener("click", function () {
-  document.getElementById("smallfetch-popup").style.display = "none";
-});
+document
+  .getElementById("close-project-popup")
+  .addEventListener("click", function () {
+    document.getElementById("smallfetch-popup").style.display = "none";
+    document.getElementById("p2p-chat-popup").style.display = "none";
+  });
 
 window.addEventListener("click", function (event) {
   let popup = document.getElementById("smallfetch-popup");
   if (event.target === popup) {
-      popup.style.display = "none";
+    popup.style.display = "none";
   }
 });
 
 document.getElementById("smallfetch-gitrepo").onclick = function () {
   location.href = "https://github.com/aem2231/smallfetch";
-}
-
-
+};
